ffe643044f590d5bd0f38d613691af5de9ba8393
adarsh-2425 <you@example.comadarsh.lol2425@gmail.com>
2023-09-21 09:35:36 +0530

    login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,10 @@ import { Model } from 'mongoose';
 import { UsernameService } from './username.service';
 import { LoginInterface } from './interfaces/login.interface';
 import * as bcrypt from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import * as jwt from 'jsonwebtoken';
+import { UnauthorizedException } from '@nestjs/common/exceptions';
+import { UserInterface } from './interfaces/user.interface';
 
 @Injectable()
 export class AuthService {
@@ -24,6 +28,29 @@ export class AuthService {
         user.password = await bcrypt.hash(user.password, 10);
 
         return await newUser.save();
+    }
+
+    // Login Service
+    async login(loginDto: LoginDto): Promise<{token: string, user: UserInterface}> {
+
+        const user = await this.userModel.findOne({email: loginDto.email});
+
+        if (!user) {
+            throw new UnauthorizedException('User does not exist. Check your email')
+        }
+
+        const isPasswordValid = await bcrypt.compare(loginDto.password, user.password);
+
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid password');
+        }
+
+        // Generate JWT Token
+        const token = jwt.sign({sub: user._id, username: user.username}, 'secret', {
+            expiresIn: '5h'
+        })
+
+        return { token, user: {id: user.id, username: user.username}};
     }
 }
 
